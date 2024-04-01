6965f84bc57926eb2ce1ad0d19dc4f61a912551d
Lenchik1218 <144795372+Lenchik1218@users.noreply.github.com>
2024-04-01 23:31:14 +0300

    гшнвщн

--- lab_7_2.py
+++ lab_7_2.py
@@ -1,9 +1,29 @@
-import numpy as np
 import matplotlib.pyplot as plt
-R= 1
-t = np.arange(0, 10, 0.01)
-x = R*np.cos(t)**3
-y = R*np.sin(t)**3
-plt.axis('equal')
-plt.plot(x, y)
-plt.savefig('fig1.2.png')
+import numpy as np
+from matplotlib.animation import FuncAnimation
+
+def circle(a, t):
+    R = a * t
+    alpha = np.arange(0, 2*np.pi, 0.01)
+    x = R*np.cos(alpha)
+    y = R*np.sin(alpha)
+    return x, y
+
+def animate(i):
+    ball.set_data(circle(a = 0.1, t=i))
+
+
+if __name__ == '__main__':
+    plt.axis('equal')
+    fig, ax = plt.subplots()
+    ball, = plt.plot([], [], '-', color='k')
+
+    ax.set_xlim(-6, 6)
+    ax.set_ylim(-6, 6)
+
+    ani = FuncAnimation(fig, 
+                        animate,
+                        frames=20,
+                        interval=50
+                        )
+    ani.save('Lab_7_task_2.gif')
